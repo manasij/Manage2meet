7277a7abae1afabcb637fd9b6b8226b950682ad6
Manasi Jayaraman <manasijayaraman@gmail.com>
2015-11-22 20:03:58 +0800

    Login Page and registration

---
+++ Manage2Meet/app/src/main/java/project/group/android/manage2meet/GroupDisplay.java
@@ -0,0 +1,11 @@
+package project.group.android.manage2meet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GroupDisplay extends Activity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_groups);
+    }
+}
---
+++ Manage2Meet/app/src/main/java/project/group/android/manage2meet/LoginActivity.java
@@ -0,0 +1,152 @@
+package project.group.android.manage2meet;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText passwordText, usernameText;
+    private Button loginButton, registerButton;
+    public static final String LOGIN_URL = "http://i.cs.hku.hk/~kasliwal/Android/login.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        passwordText = (EditText) findViewById(R.id.enterPassword);
+        usernameText = (EditText) findViewById(R.id.enterUsername);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerButton = (Button) findViewById(R.id.registerButton);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), Registration.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void loginUser() {
+        String username = usernameText.getText().toString().trim().toLowerCase();
+        String password = passwordText.getText().toString().trim().toLowerCase();
+
+        login(username, password);
+    }
+    private void login(String username, String password) {
+        class LoginUser extends AsyncTask<String, Void, String>{
+            ProgressDialog loading;
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(LoginActivity.this, "Please Wait",null, true, true);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(),s,Toast.LENGTH_LONG).show();
+
+                if(s=="success") {
+                    Intent intent = new Intent(getBaseContext(), GroupDisplay.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap();
+                data.put("username",params[0]);
+                data.put("password",params[1]);
+
+                String result = sendPostRequest(LOGIN_URL, data);
+
+                return  result;
+            }
+        }
+
+        LoginUser user = new LoginUser();
+        user.execute(username, password);
+    }
+    public String sendPostRequest(String requestURL,
+                                  HashMap<String, String> postDataParams) {
+
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                response = br.readLine();
+            }
+            else {
+                response="Error Registering";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+}
+
